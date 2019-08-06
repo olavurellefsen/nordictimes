7660d9583228daf41fac2918317f50908beebb8a
Raffaele Salvucci <rafs@dtu.dk>
2019-08-06 17:13:31 +0200

    New results added

---
+++ src/data/scenarioCombinations.js
@@ -0,0 +1,19 @@
+export default {scenarioCombinations : {scenarioOptions : [
+{ "id": 0, "name": "Reference Bio CCS", "short_description": "Reference Bio CCS", "ultra_short_description": "" },
+{ "id": 1, "name": "Reference Bio", "short_description": "Reference Bio", "ultra_short_description": "" },
+{ "id": 2, "name": "Reference CCS", "short_description": "Reference CCS", "ultra_short_description": "" },
+{ "id": 3, "name": "Reference", "short_description": "Reference", "ultra_short_description": "" },
+{ "id": 4, "name": "Share Optimism Bio CCS", "short_description": "Share Optimism Bio CCS", "ultra_short_description": "" },
+{ "id": 5, "name": "Share Optimism Bio", "short_description": "Share Optimism Bio", "ultra_short_description": "" },
+{ "id": 6, "name": "Share Optimism CCS", "short_description": "Share Optimism CCS", "ultra_short_description": "" },
+{ "id": 7, "name": "Share Optimism", "short_description": "Share Optimism", "ultra_short_description": "" },
+{ "id": 8, "name": "Technology Optimism Bio CCS", "short_description": "Technology Optimism Bio CCS", "ultra_short_description": "" },
+{ "id": 9, "name": "Technology Optimism Bio", "short_description": "Technology Optimism Bio", "ultra_short_description": "" },
+{ "id": 10, "name": "Technology Optimism CCS", "short_description": "Technology Optimism CCS", "ultra_short_description": "" },
+{ "id": 11, "name": "Technology Optimism", "short_description": "Technology Optimism", "ultra_short_description": "" }
+],
+regionOptions : [
+{ "id": 0, "name": "Denmark", "country": "Denmark", "short_description": "Denmark", "ultra_short_description": "Denmark" },
+{ "id": 1, "name": "Norway", "country": "Norway", "short_description": "Norway", "ultra_short_description": "Norway" },
+{ "id": 2, "name": "Sweden", "country": "Sweden", "short_description": "Sweden", "ultra_short_description": "Sweden" }
+]}}
